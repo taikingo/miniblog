6b62ded2fd6b57b163dbf66c96dbdde351d907af
taikingo <taikingo66@gmail.com>
2024-10-08 16:21:42 +0900

    投稿日時の表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   validates :content, presence: true
   validates :content, length: { maximum: 140 }
+
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 end
