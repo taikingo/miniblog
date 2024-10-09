a94c48a90396073be600ec386ffccf5adb41cbb5
taikingo <taikingo66@gmail.com>
2024-10-10 00:38:15 +0900

    ユーザーと投稿の紐付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   validates :content, presence: true
   validates :content, length: { maximum: 140 }
 
+  belongs_to :user
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+
+  def has_written?(post)
+    posts.exists?(id: post.id)
+  end
 end
