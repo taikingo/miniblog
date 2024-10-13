d532779727dafc8a2f4c10d8bf9df22ecb35314e
taikingo <taikingo66@gmail.com>
2024-10-13 23:01:30 +0900

    記事一覧ページの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(user: :profile).all
   end
 
   def show
