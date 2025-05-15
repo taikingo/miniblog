c4ac8ea66193762f84f93b873e0e37a7eae97ff4
taikingo <taikingo66@gmail.com>
2025-05-15 18:26:54 +0900

    エラー修正

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,11 @@ module.exports = {
       autoprefixer: {
         flexbox: 'no-2009'
       },
-      stage: 3
+      stage: 3,
+      features: {
+        'custom-properties': false
+      }
     })
   ]
 }
+
